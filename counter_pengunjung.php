b35c2466804cccd60110d68f7ad2a9ead2c28dc3
Muhammad Mahsum Farhani <FarhanHanz987gmail.com>
2026-04-22 13:59:23 +0700

    praktikum 1

--- counter_pengunjung.php
+++ counter_pengunjung.php
@@ -1,27 +1,17 @@
 <?php
 
 class Pengunjung {
-    public static $jumlah = 0; 
+    public static $jumlah = 0;
 
-    public function __construct() { 
-        self::$jumlah++; 
+    public function __construct() {
+        self::$jumlah++;
     }
-
-    public static function reset() { 
-        self::$jumlah = 0; 
-    } 
 }
 
-$p1 = new Pengunjung(); 
-$p2 = new Pengunjung(); 
-$p3 = new Pengunjung(); 
-$p4 = new Pengunjung();
-$p5 = new Pengunjung();
+$p1 = new Pengunjung();
+$p2 = new Pengunjung();
+$p3 = new Pengunjung();
 
-echo "Jumlah Pengunjung (Sebelum Reset): " . Pengunjung::$jumlah . "<br>"; // [cite: 233, 237]
-
-Pengunjung::reset();
-
-echo "Jumlah Pengunjung (Sesudah Reset): " . Pengunjung::$jumlah . "<br>"; // 
+echo "Jumlah Pengunjung: " . Pengunjung::$jumlah;
 
 ?>
